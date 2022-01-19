4cd76ce41789ecb8bc89768913571e89649f95a6
LifeofLight <78275747+LifeofLight@users.noreply.github.com>
2022-01-19 12:02:26 +0700

    icon

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
-//import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = (props) => (
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
+
 
 const HomeScreen = ({navigation}) => {
+
+
+React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft:()=>(
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+          title="Menu"
+          iconName="Menu"
+          onPress={() => alert('เมนู')} />
+        </HeaderButtons>
+      ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+          title="register"
+          iconName="person-add"
+          onPress={() => alert('ลงทะเบียน')} />
+        </HeaderButtons>
+        
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-     {/*  <Ionicons name='home-outline' size={30} color='#eee123'/>*/}
+     <Ionicons name='home-outline' size={30} color='#456123'/>
       <Text>หน้าหลัก</Text>
       <Button
         title="Go to About"
