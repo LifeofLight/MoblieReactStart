0994559201406d2484520e3c03ec3ecc8fce9f2c
LifeofLight <78275747+LifeofLight@users.noreply.github.com>
2022-02-23 12:06:07 +0700

    2/23/2022 from

--- screen/ProductScreen.js
+++ screen/ProductScreen.js
@@ -3,7 +3,7 @@ import {StyleSheet, View,ActivityIndicator,FlatList} from 'react-native';
 import { Container,Text, Header, Content, List, ListItem, Thumbnail, Left, Body, Right,Button } from 'native-base';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
-
+import {useFocusEffect}from'@react-navigation/native';
 import {
   HeaderButtons,
   HeaderButton,
@@ -40,18 +40,53 @@ const ProductScreen = ({navigation}) => {
 
   const [product,setProduct]=useState([]);
   //useEffect  จะทำงานเมื่อคลิกเมนูสินค้า
+  const [loading,setLoading]=useState(false);
 
-  useEffect( async ()=>{
-    // getData() for get data from backend
+  let cancelToken;
+
     const getData = async ()=>{
-      const res = await axios.get('https://api.codingthailand.com/api/course')
+      setLoading(true);
+      const res = await axios.get('https://api.codingthailand.com/api/course',{
+        cancelToken : cancelToken.token
+      });
       //alert(JSON.stringify(res.data.data))//data ตัวแรกคือฟังชั่นของ axios ตัวที่ 2คือตัวของ arrayobj **ต้องแปลงไฟล์ JSON ด้วย**
       setProduct(res.data.data);
+      setLoading(false);
     }
-        getData();
-    console.log(product)
-  },[])
 
+    //ทุกๆครั้งที่เข้าหน้า product หรือ focus ที่หน้า product
+    //เราจะให้ไปดึงข้อมูลที่ server ตลอดเวลา
+    useFocusEffect(
+      //usecallback เอาไว้ optimize ฟังก์ชัน เพื่อไม่ให้ re-render ของ child component
+      React.useCallback(()=>{
+        cancelToken=axios.CancelToken.source();
+        getData();
+        return()=>{
+         // alert('Exit ProductScreen');
+          cancelToken.cancel();
+        }
+      },[])
+    )
+  // useEffect(()=>{
+  //   // getData() for get data from backend
+  //       getData();
+  // },[])
+
+  if (loading ===true){
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator
+        color='#afbc'
+        size='large'
+
+        />
+      </View>
+    );
+  }
+
+  const _onRefresh =()=>{
+    getData();
+  }
 
   return (
     <View >
@@ -60,9 +95,17 @@ const ProductScreen = ({navigation}) => {
       data={product}
       //keyExtractor primary key(คีย์หลัก)
       keyExtractor={(item,index)=>item.id.toString()}
+      //pull to refresh
+      onRefresh={_onRefresh}
+      refreshing={loading}//ถ้า refreshing เป็น true คือจะรอให้ refresh data
       //renderItem สำหรับ render UI ที่จะให้ user มองเห็น (return jsx)
       renderItem ={({item})=>(
-         <ListItem thumbnail>
+         <ListItem thumbnail onPress={()=>{
+           navigation.navigate('Detail',{
+             id:item.id,
+             title:item.title //นำค่า title จาก backend ส่งให้ตัวแปร title ไปยัง อีกหน้า
+           });
+         }}>
               <Left>
                 <Thumbnail square source={{ uri: item.picture }} />
               </Left>
--- screen/RegisterScreen.js
+++ screen/RegisterScreen.js
@@ -1,11 +1,99 @@
-import { Text, View } from 'react-native'
+import { Text, View ,Component} from 'react-native'
 import React from 'react'
+import { Container,Icon, Header, Content, Form, Item, Input, Label, Button } from 'native-base';
+import axios from 'axios';
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
+
+
+const validateSchema = Yup.object().shape({
+  name: Yup.string().required('กรุณาป้อนชื่อสกุล'),
+  email: Yup.string().email('รูปแบบอีเมลไม่ถูกต้อง').required('กรุณากรอกใหม่'),
+  password: Yup.string().min(5,'รหัสผ่านต้อง 5 ตัวอักษรขึ้นไป').required('กรุณาป้อนรหัสผ่าน'),
+});
 
 const RegisterScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>ลงทะเบียร</Text>
-    </View>
+    <Container>
+      <Content padder>
+      <Formik
+       initialValues={{
+         //ต่าเริ่มต้นของข้อมูลโดยกำหนดให้ตรงกับ backend
+         name: '',
+         email: '',
+         password: '',
+       }}
+       validationSchema={validateSchema}
+       //เมื่อคลิกปุ่ม register ให้ทำงานส่วน
+       onSubmit={(values) => {
+         // same shape as initial values
+         console.log(values);
+       }}
+     >
+       {/*errors ใช้สำหรับการตรวจสอบ err ที่เกิดขึ้น (ถ้าผู้ใช้ไม่กรอกข้อมูลจะให้ err อะไรเกิดขึ้น)*/}
+       {/*touched เมื่อผู้ใช้ไปกดที่ name และเลื่อนเมาส์ออกไปด้านนอกช่อง input โดยไม่กรอกข้อมูล */}
+
+       {({ errors, touched,values,handleChange,handleBlur }) => (          
+       <Form>
+         {/* กำหนดให้มีเส้นสีแดงถ้าผู้ใช้ไม่กรอกข้อมูลชื่อ*/}
+          <Item fixedLabel error={errors.name && touched.name?true:false}>
+            <Label>Name</Label>
+            <Input 
+              value={values.name}
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+            />
+            {errors.name && touched.name && <Icon name= 'close-circle'/>}
+            
+          </Item>
+          {
+              errors.name && touched.name &&(
+                <Item>
+                  <Label style={{color:'red'}}>{errors.name}</Label>
+                </Item>
+              )
+            }
+          <Item fixedLabel error={errors.email && touched.email?true:false}>
+            <Label>Email</Label>
+            <Input 
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+            />
+            {errors.email && touched.email && <Icon name= 'close-circle'/>}
+          </Item>
+          {
+              errors.email && touched.email &&(
+                <Item>
+                  <Label style={{color:'red'}}>{errors.email}</Label>
+                </Item>
+              )
+            }
+           <Item fixedLabel error={errors.password && touched.password?true:false}>
+            <Label>Password</Label>
+            <Input 
+              value={values.password}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+            />
+            {errors.password && touched.password && <Icon name= 'close-circle'/>}
+
+          </Item>
+          {
+              errors.password && touched.password &&(
+                <Item>
+                  <Label style={{color:'red'}}>{errors.password}</Label>
+                </Item>
+              )
+            }
+          <Button block large style={{marginTop:30,backgroundColor:'#afbc'}}>
+          <Text style={{fontSize:15,fontWeight:'bold'}}>Register</Text>
+        </Button>
+        </Form>
+       )}
+     </Formik>
+      </Content>
+    </Container>
   )
 }
 
